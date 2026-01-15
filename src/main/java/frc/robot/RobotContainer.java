daf78374ecd41c87b86f62dd73227b0f9d9731d1
AxolotlGames <h@townsendwatkins.com>
2026-01-15 13:00:04 -0500

    Updated RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,13 +10,22 @@ import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-public class RobotContainer {
+public class RobotContainer {  
+	private final Shooter shooter;
+  
   public RobotContainer() {
     configureBindings();
   }
   
   private void configureBindings() {
-    
+    // Change these to use actual swerve values converted to Vector3
+    private final Supplier<Vector3> robotVelocity = () -> new Vector3(2.0,0.0,0.0);
+    private final DoubleSupplier angularVelocityRadians = () -> 2;
+    private final Supplier<Vector3> robotPosition = () -> new Vector3(2*time,0,0);
+    private final DoubleSupplier heading = () -> 2*time;
+    private final Vector3 goalPosition = new Vector3(0,3,2);
+
+    shooter = new Shooter(robotVelocity, angularVelocityRadians, robotPosition, heading, goalPosition);
   }
 
   public Command getAutonomousCommand() {
