4c8efb9cf5e9417355098f292189589645789c10
AxolotlGames <h@townsendwatkins.com>
2026-01-15 12:35:32 -0500

    Added the rest of the test files

---
+++ test/Constants.java
@@ -0,0 +1,14 @@
+public final class Constants {
+  public static final class ShooterConstants {
+    // Robot constants
+    public static final Vector3 turretPlacement = new Vector3(0.2,0.2,0.4);
+    public static final double pitch = Math.PI / 4;
+    public static final double maxShotSpeed = 20;
+    public static final double minShotSpeed = 1;
+    // Number of iterations in the binary search
+    // 54 is expirimentally determined to reach maximum accuracy
+    // 64 should be a 100% garentee
+    public static final int searchDepth = 10;
+    public static final double gravity = 9.81;
+  }
+}
---
+++ test/Shooter.java
@@ -0,0 +1,157 @@
+import java.util.function.Supplier;
+import java.util.function.DoubleSupplier;
+import java.util.function.Consumer;
+import java.util.ArrayList;
+
+public class Shooter {
+  private final Supplier<Vector3> robotVelocity;
+  private final DoubleSupplier angularVelocityRadians;
+  private final Supplier<Vector3> robotPosition;
+  private final DoubleSupplier heading;
+  
+  private Vector3 goalPosition;
+  private double time = 0;
+  
+  private ArrayList<Double> times = new ArrayList<Double>();
+  private ArrayList<Double> shotSpeeds = new ArrayList<Double>();
+  private ArrayList<Double> angles = new ArrayList<Double>();
+  private ArrayList<Vector3> positions = new ArrayList<Vector3>();
+  private ArrayList<Vector3> velocities = new ArrayList<Vector3>();
+  private ArrayList<Double> headings = new ArrayList<Double>();
+  private ArrayList<Double> angularVelocities = new ArrayList<Double>();
+  
+  public Shooter(Supplier<Vector3> _robotVelocity, 
+                 DoubleSupplier _angularVelocityRadians,
+                 Supplier<Vector3> _robotPosition,
+                 DoubleSupplier _heading,
+                 Vector3 _goalPosition) {
+                  
+    robotVelocity = _robotVelocity;
+    angularVelocityRadians = _angularVelocityRadians;
+    robotPosition = _robotPosition;
+    heading = _heading;
+    goalPosition = _goalPosition;
+  }
+  
+  public void setGoal(Vector3 _goalPosition) {
+    goalPosition = _goalPosition;
+  }
+  
+  private double evalShotSpeed(double speed) {
+    Vector3 position = Vector3.add(robotPosition.get(),
+                                   Vector3.rotate(Constants.ShooterConstants.turretPlacement,
+                                                  Vector3.origin(),
+                                                  heading.getAsDouble()));
+    double turretDistance = Constants.ShooterConstants.turretPlacement.get2D().length();
+    Vector3 tangent = Vector3.scale(Vector3.rotate(Constants.ShooterConstants.turretPlacement.get2D(),
+                                                   Vector3.origin(),
+                                                   Math.PI/2 + heading.getAsDouble()),
+                                    1.0/turretDistance);
+    Vector3 velocity = Vector3.add(robotVelocity.get(),
+                                   Vector3.scale(tangent,angularVelocityRadians.getAsDouble()*turretDistance));
+    
+    double a = speed * Math.cos(Constants.ShooterConstants.pitch);
+    double c = velocity.length();
+
+    Vector3 idealVector = Vector3.subtract(goalPosition, position).get2D();
+    double targetAngle = idealVector.getCounterclockwiseAngle();
+    double driveAngle = velocity.getCounterclockwiseAngle();
+    double A = (targetAngle - driveAngle) % (2 * Math.PI);
+    double C = Math.asin((c*Math.sin(A)) / a);
+    double B = Math.PI - A - C;
+
+    double b = 0;
+    if (A == 0) {
+      b = a + c;
+    }
+    else if (A == Math.PI) {
+      b = a - c;
+    }
+    else {
+      b = (a*Math.sin(B))/Math.sin(A);
+    }
+
+    double distance = idealVector.get2D().length();
+    double time = distance / b;
+    return speed*time*Math.sin(Constants.ShooterConstants.pitch) - (Constants.ShooterConstants.gravity/2)*time*time - goalPosition.z + Constants.ShooterConstants.turretPlacement.z;
+  }
+
+  public double getIdealShotSpeed() {
+    double min = Constants.ShooterConstants.minShotSpeed;
+    double max = Constants.ShooterConstants.maxShotSpeed;
+    for (int i = 0; i < Constants.ShooterConstants.searchDepth; i++) {
+      double temp = (min+max)/2;
+      double current = evalShotSpeed(temp);
+      if (current < 0) {
+        min = temp;
+      }
+      else {
+        max = temp;
+      }
+    }
+    return max;
+  }
+
+  public double getIdealAngle(double speed) {
+    Vector3 position = Vector3.add(robotPosition.get(),
+                                   Vector3.rotate(Constants.ShooterConstants.turretPlacement,
+                                                  Vector3.origin(),
+                                                  heading.getAsDouble()));
+    double turretDistance = Constants.ShooterConstants.turretPlacement.get2D().length();
+    Vector3 tangent = Vector3.scale(Vector3.rotate(Constants.ShooterConstants.turretPlacement.get2D(),
+                                                   Vector3.origin(),
+                                                   Math.PI/2 + heading.getAsDouble()),
+                                    1.0/turretDistance);
+    Vector3 velocity = Vector3.add(robotVelocity.get(),
+                                   Vector3.scale(tangent,angularVelocityRadians.getAsDouble()*turretDistance));
+    
+    double a = speed * Math.cos(Constants.ShooterConstants.pitch);
+    double c = velocity.length();
+
+    Vector3 idealVector = Vector3.subtract(goalPosition, position).get2D();
+    double targetAngle = idealVector.getCounterclockwiseAngle();
+    double driveAngle = velocity.getCounterclockwiseAngle();
+    double A = (targetAngle - driveAngle) % (2 * Math.PI);
+    double C = Math.asin((c*Math.sin(A)) / a);
+
+    return targetAngle + C + heading.getAsDouble();
+  }
+  
+  public void log(double deltaTime) {
+    double idealShotSpeed = getIdealShotSpeed();
+    double idealAngle = getIdealAngle(idealShotSpeed);
+    
+    // Replace with actual or simulated values
+    double shotSpeed = idealShotSpeed * 1.0;
+    double angle = idealAngle * 1.0;
+    
+    time += deltaTime;
+    times.add(time);
+    shotSpeeds.add(shotSpeed);
+    angles.add(angle);
+    positions.add(robotPosition.get());
+    velocities.add(robotVelocity.get());
+    headings.add(heading.getAsDouble());
+    angularVelocities.add(angularVelocityRadians.getAsDouble());
+  }
+  
+  public void logSim() {
+    printList("t_list",times,(Double item) -> System.out.print(item));
+    printList("s_list",shotSpeeds,(Double item) -> System.out.print(item));
+    printList("a_ngleList",angles,(Double item) -> System.out.print(item));
+    printList("p_ositionList",positions,(Vector3 item) -> item.print());
+    printList("v_elocityList",velocities,(Vector3 item) -> item.print());
+    printList("h_eadingList",headings,(Double item) -> System.out.print(item));
+    printList("a_ngularVelocityList",angularVelocities,(Double item) -> System.out.print(item));
+  }
+  
+  private <T> void printList(String name, ArrayList<T> list, Consumer<T> consumer) {
+    System.out.print(name.charAt(0)+"_{"+name.substring(2)+"}=\\left[");
+    for (int i = 0; i < list.size()-1; i++) {
+      consumer.accept(list.get(i));
+      System.out.print(",");
+    }
+    consumer.accept(list.get(list.size()-1));
+    System.out.println("\\right]");
+  }
+}
