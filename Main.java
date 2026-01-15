1ff6ef46d96d0987653bfce1d0d823e867d497d0
AxolotlGames <h@townsendwatkins.com>
2026-01-15 11:07:21 -0500

    Added files

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.function.Supplier;
+import java.util.function.DoubleSupplier;
+
+public class Main {
+  private double time;
+  
+	public void main(String[] args) {
+	  time = 0;
+	  
+    Supplier<Vector3> robotVelocity = () -> new Vector3(2.0,0.0,0.0);
+    DoubleSupplier angularVelocityRadians = () -> 2;
+    Supplier<Vector3> robotPosition = () -> new Vector3(2*time,0,0);
+    DoubleSupplier heading = () -> 2*time;
+    Vector3 goalPosition = new Vector3(0,3,2);
+    
+		Shooter shooter = new Shooter(robotVelocity, angularVelocityRadians, robotPosition, heading, goalPosition);
+		
+		for (double i=0; i<=2; i+=0.05) {
+		  shooter.log(0.05);
+		  time += 0.05;
+		}
+		
+		shooter.logSim();
+	}
+}
